2605776f80ba2d859f2ad9fd6fdbd09df0b6d71e
kepe3100c1 <require3100c@gmail.com>
2015-12-12 08:03:32 +0000

    tyoi

--- sandbox/prac_dammy_01.cpp
+++ sandbox/prac_dammy_01.cpp
@@ -3,12 +3,5 @@
 using namespace std;
  
 int main(){
-    int a;
-    string b;
-    cout << "How many have you ticken ?" << endl;
-    cin >> a;
-    cout << "What are you have a talent ?" << endl;
-    cin >> b;
-    cout << "a=" << a << endl << "b=" << b << endl;
-    return 0;
+
 }
